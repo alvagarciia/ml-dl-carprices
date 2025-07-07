7911080b3850cf92d79fea7e33a6f1c23ddd2558
Alvaro Garcia <146588674+alvagarciia@users.noreply.github.com>
2025-07-07 15:42:18 -0500

    update to v2: fix button/state problem

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,27 @@ import keras
 from scripts.ml_script import KFoldTargetEncoder
 from sklearn.ensemble import RandomForestRegressor
 
+# To deal with streamlit sessions and states
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = {1:False, 2:False, 3:False}
+
+def clicked(button):
+    st.session_state.clicked[button] = True
+    if button == 1 and st.session_state.clicked[2] == True:
+        st.session_state.clicked[2] = False
+    elif button == 2 and st.session_state.clicked[1] == True:
+        st.session_state.clicked[1] = False
+
+
 st.title('Used Car Price Predictor')
 
 st.markdown('##### Goal: Compare ML and DL models to predict used car prices based on real-world vehicle data.')
 
 st.caption('Fill out the following information to get your prediction. Leave blank if unknown.')
-# input_data = None
-# data_entered = False
-data_entered = True
+input_data = None
+data_entered = False
+m = 'a'
+# data_entered = True
 input_data = pd.DataFrame([{
     "milage": 7008.0,
     "accident": "Unknown",
@@ -37,45 +50,47 @@ with st.expander("Your Car Info Goes Here"):
     clean_title = st.selectbox("Clean Title", ["Yes", "No"])
     clean_title = 'Missing' if clean_title == 'No' else clean_title
 
-    milage = st.number_input("Mileage", step=1)
+    milage = st.number_input("Mileage", step=0.1)
     
-    age = st.number_input("Car Age (Years)", step=1)
+    age = st.number_input("Car Age (Years, as of 2025)", step=1)
 
-    hp = st.number_input("Engine Horse Power", step=1)
-    engine_liter = st.number_input("Engine Total Volume (L)", step=1)
-    engine_cyl = st.number_input("Engine Number of Cylinders", step=1)
+    hp = st.number_input("Engine Horse Power", step=0.1)
+    engine_liter = st.number_input("Engine Total Volume (L)", step=0.1)
+    engine_cyl = st.number_input("Engine Number of Cylinders", step=0.1)
 
     fuel_type = st.selectbox("Fuel Type", ["–", "Gasoline", "Hybrid", "Electric", "Diesel", "Plug-In Hybrid", "E85 Flex Fuel"])
 
     trans_type = st.selectbox("Transmission Type", ["–", "auto", "manual", "dual"])
     trans_type = 'unknown' if trans_type == '–' else trans_type
-    trans_spd = st.number_input("Number of Gears", step=1)
+    trans_spd = st.number_input("Number of Gears", step=0.1)
 
 
-    if st.button('Submit'):
-        input_data = {
-            "brand_model": [brand_model],
-            "accident": [accident],
-            "clean_title": [clean_title],
-            "milage": [milage],
-            "car_age": [age],
-            "engine_hp": [hp],
-            "engine_liter": [engine_liter],
-            "engine_cyl": [engine_cyl],
-            "fuel_type": [fuel_type],
-            "trans_spd": [trans_spd],
-            "trans_type": [trans_type]
-        }
+    st.button('Submit', on_click = clicked, args=[3])
+    if st.session_state.clicked[3]:
+        input_data = pd.DataFrame([{
+            "brand_model": brand_model,
+            "accident": accident,
+            "clean_title": clean_title,
+            "milage": milage,
+            "car_age": age,
+            "engine_hp": hp,
+            "engine_liter": engine_liter,
+            "engine_cyl": engine_cyl,
+            "fuel_type": fuel_type,
+            "trans_spd": trans_spd,
+            "trans_type": trans_type
+        }])
         data_entered = True
+        # m = m
 
-
-m = 'dl'
 
 with st.sidebar:
     st.header('Toggle Model')
-    if st.button('ML XGB Model'):
+    st.button('ML XGB Model', on_click = clicked, args=[1])
+    if st.session_state.clicked[1]:
         m = 'ml'
-    if st.button('DL Keras Model'):
+    st.button('DL Keras Model', on_click = clicked, args=[2])
+    if st.session_state.clicked[2]:
         m = 'dl'
 
 st.divider()
